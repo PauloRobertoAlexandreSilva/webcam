092b876d89b8f84254e0dfdd0dae750eb4df69f2
Paulo Roberto Alexandre da Silva <beto@tjrj.jus.br>
2026-09-18 14:27:15 -0300

    decode html code

--- scr/script.js
+++ scr/script.js
@@ -43,14 +43,22 @@ function fileExists(url) {
     return http.status != 404;
 }
 
+function decodeHtmlEntities(str) {
+    const textarea = document.createElement('textarea');
+    textarea.innerHTML = str;
+    return textarea.value;
+}
+
 function Descriptografar(base64Str) {
     // Convert key and IV to WordArray
     const key = CryptoJS.enc.Utf8.parse("ESAJ-DITEC092026");
     const iv = CryptoJS.enc.Utf8.parse("1234567890ABCDEF");
+    const decodedText = decodeHtmlEntities(base64Str);
 
+    
     // Decrypt
     const decrypted = CryptoJS.AES.decrypt(
-        { ciphertext: CryptoJS.enc.Base64.parse(base64Str) },
+        { ciphertext: CryptoJS.enc.Base64.parse(decodedText) },
         key,
         {
             iv: iv,
